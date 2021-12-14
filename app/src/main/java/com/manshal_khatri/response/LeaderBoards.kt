c764d09e908b5817a954e75e7c54d7510ebcfa15
Manshal <manshalkhatri51@gmail.com>
2021-12-14 17:47:04 +0530

    before new Leaderboard design

--- app/src/main/java/com/manshal_khatri/response/LeaderBoards.kt
+++ app/src/main/java/com/manshal_khatri/response/LeaderBoards.kt
@@ -11,14 +11,14 @@ import kotlin.concurrent.schedule
 
 class LeaderBoards : AppCompatActivity() {
 
-    lateinit var highScore1:TextView
+   /* lateinit var highScore1:TextView
     lateinit var topPlayer1:TextView
     lateinit var highScore2:TextView
     lateinit var topPlayer2:TextView
     lateinit var highScore3:TextView
     lateinit var topPlayer3:TextView
     lateinit var highScore4:TextView
-    lateinit var topPlayer4:TextView
+    lateinit var topPlayer4:TextView*/
     lateinit var sharedPreferences: SharedPreferences
 
 
@@ -28,7 +28,7 @@ class LeaderBoards : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leader_boards)
 
-        highScore1=findViewById(R.id.scoreValBeginner)
+       /* highScore1=findViewById(R.id.scoreValBeginner)
         topPlayer1=findViewById(R.id.topPlayerBeginner)
         highScore2=findViewById(R.id.scoreValNormal)
         topPlayer2=findViewById(R.id.topPlayerNormal)
@@ -37,11 +37,11 @@ class LeaderBoards : AppCompatActivity() {
         highScore4=findViewById(R.id.scoreValMaster)
         topPlayer4=findViewById(R.id.topPlayerMaster)
         val highScore = arrayListOf<TextView>(highScore1,highScore2,highScore3,highScore4)
-        val topPlayer = arrayListOf<TextView>(topPlayer1,topPlayer2,topPlayer3,topPlayer4)
+        val topPlayer = arrayListOf<TextView>(topPlayer1,topPlayer2,topPlayer3,topPlayer4)*/
         sharedPreferences=getSharedPreferences(getString(R.string.highScore), MODE_PRIVATE)
-        for(i in 1 until 5) {
-            highScore[i-1].text=sharedPreferences.getInt("highScore$i",0).toString()
-            topPlayer[i-1].text=sharedPreferences.getString("topPlayer$i","unknown")
+        for(i in 2 until 6) {
+           /* highScore[i-2].text=sharedPreferences.getInt("highScore$i",0).toString()
+            topPlayer[i-2].text=sharedPreferences.getString("topPlayer$i","unknown")*/
         }
 
     }
